6f96c59ff68dcc2bad6dd82cf148c0363f3c401a
Guillaume Jacquart <guillaume.jacquart@live.fr>
2016-12-07 18:43:06 +0100

    Fix case on file requiring.

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const request = require('request');
 const LocalStrategy = require('passport-local').Strategy;
 
-const User = require('../models/User');
+const User = require('../models/user');
 
 passport.serializeUser((user, done) => {
 	console.log(user);
